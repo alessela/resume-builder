828850e0e9b049419cd26b01b86531238416122a
Aless Saulea <alessa2000@yahoo.com>
2025-04-06 10:09:50 +0300

    added resume form

--- src/components/ResumeForm/ResumeForm.tsx
+++ src/components/ResumeForm/ResumeForm.tsx
@@ -7,6 +7,9 @@ const ResumeForm = () => {
     const [formData, setFormData] = useState({
         firstName: "",
         lastName: "",
+        email: "",
+        title: "",
+        summary: ""
     })
 
     const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -27,10 +30,40 @@ const ResumeForm = () => {
                               onChange={handleChange}
                               required>
                 </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
                 <Form.Label>Last name</Form.Label>
                 <Form.Control type="text"
                               name="lastName"
                               value={formData.lastName}
+                              onChange={handleChange}
+                              required>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email"
+                              name="email"
+                              value={formData.email}
+                              onChange={handleChange}
+                              required>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+                <Form.Control type="text"
+                              name="title"
+                              value={formData.title}
+                              onChange={handleChange}
+                              required>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group className="mb-3">
+            <Form.Label>Summary</Form.Label>
+                <Form.Control as="textarea"
+                              rows={3}
+                              name="summary"
+                              value={formData.summary}
                               onChange={handleChange}
                               required>
                 </Form.Control>
